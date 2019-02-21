62c4c5a8a778d51e73d60b8346bf16da51542d97
Edward <edwardlshfzw@gmail.com>
2019-02-21 18:22:59 +0800

    成完app和Model代码的创建

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
