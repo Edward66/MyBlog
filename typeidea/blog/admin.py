acecb3c719a258a1ae6f4ee97e22211d249950b2
Edward <edwardlshfzw@gmail.com>
2019-02-24 20:03:49 +0800

    编写Post的后台管理

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -24,3 +24,41 @@ class TagAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator'
+    ]
+    list_display_links = []  # 用来配置哪些字段可以作为链接
+
+    list_filter = ['category', ]  # 需要通过哪些字段来过滤列表页
+    search_fields = ['title', 'category_name']
+
+    actions_on_top = True  # 动作
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True  # 编辑页面按钮
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag'
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+
+    operator.short_description = '操作'  # 指定表头的展示文案
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
