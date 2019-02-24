83bb42bed7823c3e1fe37ef03b46562c2be98106
Edward <edwardlshfzw@gmail.com>
2019-02-24 23:49:34 +0800

    用户只能看到自己的文章

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -84,3 +84,7 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):  # 用户只能看到自己的文章
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
