576b7af49045cc23a3af8eedb7b7caf3149a4dbf
Edward <edwardlshfzw@gmail.com>
2019-02-24 20:19:30 +0800

    完成comment和config的admin配置

--- typeidea/config/admin.py
+++ typeidea/config/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link, SideBar
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'href', 'status', 'weight', 'created_time')
+    fields = ('title', 'href', 'status', 'weight')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(LinkAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(SideBar)
+class SideBar(admin.ModelAdmin):
+    list_display = ('title', 'display_type', 'content', 'created_time')
+    fields = ('title', 'display_type', 'content')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(SideBar, self).save_model(request, obj, form, change)
