c5a84971aa2712e5c0c19d25eb142f64d99ff0e6
Edward <edwardlshfzw@gmail.com>
2019-03-06 20:27:19 +0800

    fix bug，新增、编辑默认是当前用户

--- typeidea/typeidea/base_admin.py
+++ typeidea/typeidea/base_admin.py
@@ -15,4 +15,5 @@ class BaseOwnerAdmin(admin.ModelAdmin):
         return qs.filter(owner=request.user)
 
     def save_model(self, request, obj, form, change):
+        obj.owner = request.user
         return super(BaseOwnerAdmin, self).save_model(request, obj, form, change)
