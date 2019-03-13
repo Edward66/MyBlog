a73a9ac7f294114d00ff7072d254f2c93999e454
Edward <edwardlshfzw@gmail.com>
2019-03-13 23:45:57 +0800

    增加友链页面

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -5,7 +5,7 @@ from blog.views import (
     IndexView, CategoryView, TagView,
     PostDetailView, SearchView, AuthorView
 )
-from config.views import links
+from config.views import LinkListView
 from .custom_site import custom_site
 
 urlpatterns = [
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'links/$', links, name='links'),
+    url(r'links/$', LinkListView.as_view(), name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
     url(r'^search/$', SearchView.as_view(), name='search'),
