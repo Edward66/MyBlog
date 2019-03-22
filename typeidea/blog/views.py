af7966a8fdfe646f7f1a3de1abb6c165d6deb8f1
Edward <edwardlshfzw@gmail.com>
2019-03-22 23:22:26 +0800

    抽象出评论模块组件

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -89,14 +89,6 @@ class PostDetailView(CommonViewMixin, DetailView):
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'comment_form': CommentForm,
-            'comment_list': Comment.get_by_target(self.request.path),
-        })
-        return context
-
 
 class SearchView(IndexView):
     def get_context_data(self):
@@ -119,6 +111,3 @@ class AuthorView(IndexView):
         queryset = super().get_queryset()
         author_id = self.kwargs.get('owner_id')
         return queryset.filter(owner_id=author_id)
-
-
-
